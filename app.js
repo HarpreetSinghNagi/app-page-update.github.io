7d5acb9c6771d6bcd569788c43f87210bb49e9bf
Harpreet Nagi <harpreetnagi456@gmail.com>
2020-09-30 18:53:25 +0530

    add
    add files

---
+++ app.js
@@ -0,0 +1,38 @@
+
+function  opeslide ( ) {
+    document.getElementById('open').style.width="100%";
+}
+
+
+function  closeslide ( ) {
+    document.getElementById('open').style.width="0";
+}
+
+
+ 
+  function changecolor(){
+       document.getElementById('color').style.background="#000000";
+       document.getElementById('color-box').style.background="#ffffff";
+      
+       
+       document.getElementById('color-text').style.color="#000000";
+       d
+
+   }
+
+
+
+//  function changecolor(){
+//         var change= document.getElementById('color').style.background="#000000"
+
+//         if(change.style.background == "#000000")
+//         {
+//             change.style.background =="deepblue"
+//         }
+//         else{
+//             change.style.background=="blue"
+//         }
+
+
+
+//  }
